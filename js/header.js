24927300eb3f1b5ed9e144464402dc1d7af4881c
chiranjiv37 <chiranjiv_rao@platy.studio>
2024-12-10 12:50:46 +0530

    css changes

--- js/header.js
+++
@@ -1,9 +0,0 @@
-// // Hamburger menu toggle functionality
-// const hamburgerMenu = document.querySelector(".hamburger-menu");
-// const mobileMenu = document.querySelector(".mobile-menu");
-
-// if (hamburgerMenu && mobileMenu) {
-//   hamburgerMenu.addEventListener("click", () => {
-//     mobileMenu.classList.toggle("active"); // Add or remove the "active" class
-//   });
-// }
